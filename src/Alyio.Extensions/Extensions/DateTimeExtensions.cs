549cec5dfd8e50913d0490c4388d878fda22fa4b
Jon X <ousiax@hotmail.com>
2025-09-24 20:11:34 +0800

    feat(datetime): Add DateTimeOffset extensions and update DateTime extensions
    - Add extension methods for converting between DateTime, DateTimeOffset, and Unix timestamps.
    - Refactor DateTimeExtensions to use file-scoped namespaces.
    - Update unit tests to cover the new DateTimeOffset functionality.

--- src/Alyio.Extensions/Extensions/DateTimeExtensions.cs
+++ src/Alyio.Extensions/Extensions/DateTimeExtensions.cs
@@ -1,95 +1,140 @@
 ﻿// MIT License
 
-namespace Alyio.Extensions
+namespace Alyio.Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="DateTime"/> type conversions and formatting.
+/// </summary>
+public static class DateTimeExtensions
 {
+    private static readonly DateTime s_epoch = new(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
     /// <summary>
-    /// Extension methods for <see cref="DateTime"/> type conversions and formatting.
+    /// Converts the <see cref="DateTime"/> object to its equivalent integer representation using the specified 'yyyyMMdd' format.
     /// </summary>
-    public static class DateTimeExtensions
+    /// <param name="datetime">The date and time value to convert.</param>
+    /// <returns>A 32-bit signed integer representing the date in 'yyyyMMdd' format.</returns>
+    public static int ToDateInt32(this DateTime datetime)
     {
-        private static readonly DateTime s_epoch = new(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        return datetime.Year * 10000 + datetime.Month * 100 + datetime.Day;
+    }
 
-        /// <summary>
-        /// Converts the <see cref="DateTime"/> object to its equivalent integer representation using the specified 'yyyyMMdd' format.
-        /// </summary>
-        /// <param name="datetime">The date and time value to convert.</param>
-        /// <returns>A 32-bit signed integer representing the date in 'yyyyMMdd' format.</returns>
-        public static int ToDateInt32(this DateTime datetime)
+    /// <summary>
+    /// Converts the <see cref="DateTime"/> object to its equivalent string representation using the specified 'yyyy-MM-dd HH:mm:ss' format.
+    /// </summary>
+    /// <param name="datetime">The date and time value to convert.</param>
+    /// <returns>The string representation of the date and time in 'yyyy-MM-dd HH:mm:ss' format.</returns>
+    public static string ToyyyyMMddHHmmss(this DateTime datetime)
+    {
+        return datetime.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+    }
+
+    /// <summary>
+    /// Converts the <see cref="DateTime"/> object to its equivalent Unix timestamp (number of seconds since Unix epoch).
+    /// </summary>
+    /// <param name="datetime">The date and time value to convert.</param>
+    /// <returns>A 64-bit signed integer representing the number of seconds since Unix epoch (1970-01-01 00:00:00 UTC).</returns>
+    public static long ToUnix(this DateTime datetime)
+    {
+        var timeSpan = datetime.ToUniversalTime().Subtract(s_epoch);
+        return (long)timeSpan.TotalSeconds;
+    }
+
+    /// <summary>
+    /// Converts a Unix timestamp (number of seconds since Unix epoch) to its equivalent <see cref="DateTime"/>.
+    /// </summary>
+    /// <param name="seconds">The Unix timestamp to convert.</param>
+    /// <returns>
+    /// A <see cref="DateTime"/> representing the local time corresponding to the given Unix timestamp, or null if:
+    /// - The resulting date is outside the valid range for <see cref="DateTime"/>
+    /// - The timestamp is negative
+    /// </returns>
+    public static DateTime? ToDateTime(this long seconds)
+    {
+        if (seconds < 0)
         {
-            return datetime.Year * 10000 + datetime.Month * 100 + datetime.Day;
+            return null;
         }
 
-        /// <summary>
-        /// Converts the <see cref="DateTime"/> object to its equivalent string representation using the specified 'yyyy-MM-dd HH:mm:ss' format.
-        /// </summary>
-        /// <param name="datetime">The date and time value to convert.</param>
-        /// <returns>The string representation of the date and time in 'yyyy-MM-dd HH:mm:ss' format.</returns>
-        public static string ToyyyyMMddHHmmss(this DateTime datetime)
+        try
         {
-            return datetime.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+            return s_epoch.AddSeconds(seconds);
+        }
+        catch (Exception ex) when (ex is ArgumentOutOfRangeException || ex is OverflowException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Converts a Unix timestamp (number of seconds since Unix epoch) to its equivalent <see cref="DateTime"/>.
+    /// </summary>
+    /// <param name="seconds">The Unix timestamp to convert.</param>
+    /// <returns>
+    /// A <see cref="DateTime"/> representing the local time corresponding to the given Unix timestamp, or null if:
+    /// - The resulting date is outside the valid range for <see cref="DateTime"/>
+    /// - The timestamp is negative
+    /// </returns>
+    public static DateTime? ToDateTime(this double seconds)
+    {
+        if (double.IsNaN(seconds) || double.IsInfinity(seconds) || seconds < 0)
+        {
+            return null;
         }
 
-        /// <summary>
-        /// Converts the <see cref="DateTime"/> object to its equivalent Unix timestamp (number of seconds since Unix epoch).
-        /// </summary>
-        /// <param name="datetime">The date and time value to convert.</param>
-        /// <returns>A 64-bit signed integer representing the number of seconds since Unix epoch (1970-01-01 00:00:00 UTC).</returns>
-        public static long ToUnix(this DateTime datetime)
+        try
         {
-            var timeSpan = datetime.ToUniversalTime().Subtract(s_epoch);
-            return (long)timeSpan.TotalSeconds;
+            return s_epoch.AddSeconds(seconds);
         }
-
-        /// <summary>
-        /// Converts a Unix timestamp (number of seconds since Unix epoch) to its equivalent <see cref="DateTime"/>.
-        /// </summary>
-        /// <param name="seconds">The Unix timestamp to convert.</param>
-        /// <returns>
-        /// A <see cref="DateTime"/> representing the local time corresponding to the given Unix timestamp, or null if:
-        /// - The resulting date is outside the valid range for <see cref="DateTime"/>
-        /// - The timestamp is negative
-        /// </returns>
-        public static DateTime? ToDateTime(this long seconds)
+        catch
         {
-            if (seconds < 0)
-            {
-                return null;
-            }
-
-            try
-            {
-                return s_epoch.AddSeconds(seconds);
-            }
-            catch (Exception ex) when (ex is ArgumentOutOfRangeException || ex is OverflowException)
-            {
-                return null;
-            }
+            return null;
         }
+    }
 
-        /// <summary>
-        /// Converts a Unix timestamp (number of seconds since Unix epoch) to its equivalent <see cref="DateTime"/>.
-        /// </summary>
-        /// <param name="seconds">The Unix timestamp to convert.</param>
-        /// <returns>
-        /// A <see cref="DateTime"/> representing the local time corresponding to the given Unix timestamp, or null if:
-        /// - The resulting date is outside the valid range for <see cref="DateTime"/>
-        /// - The timestamp is negative
-        /// </returns>
-        public static DateTime? ToDateTime(this double seconds)
-        {
-            if (double.IsNaN(seconds) || double.IsInfinity(seconds) || seconds < 0)
-            {
-                return null;
-            }
+    /// <summary>
+    /// Converts the <see cref="DateTime"/> object to its equivalent <see cref="DateTimeOffset"/> object.
+    /// </summary>
+    /// <param name="datetime">The date and time value to convert.</param>
+    /// <returns>A <see cref="DateTimeOffset"/> object representing the date and time in the local time zone.</returns>
+    /// <remarks>
+    /// The resulting <see cref="DateTimeOffset"/> will use the offset specified by the <see cref="DateTime.Kind"/> property (e.g., Utc or Local)., otherwise if the <see cref="DateTime.Kind"/> is <see cref="DateTimeKind.Unspecified"/>, the local time zone offset will be applied.
+    /// </remarks>
+    public static DateTimeOffset ToDateTimeOffset(this DateTime datetime)
+    {
+        return new DateTimeOffset(datetime);
+    }
 
-            try
-            {
-                return s_epoch.AddSeconds(seconds);
-            }
-            catch
-            {
-                return null;
-            }
-        }
+    /// <summary>
+    /// Converts the <see cref="DateTimeOffset"/> object to its equivalent <see cref="DateTime"/> object.
+    /// </summary>
+    /// <param name="datetimeOffset">The date and time value to convert.</param>
+    /// <returns>A <see cref="DateTime"/> object representing the date and time in the UTC time zone.</returns>
+    public static DateTime ToDateTime(this DateTimeOffset datetimeOffset)
+    {
+        return datetimeOffset.UtcDateTime;
+    }
+
+    /// <summary>
+    /// Converts the <see cref="DateTimeOffset"/> object to its equivalent Unix timestamp (number of seconds since Unix epoch).
+    /// </summary>
+    /// <param name="datetimeOffset">The date and time value to convert.</param>
+    /// <returns>A 64-bit signed integer representing the number of seconds since Unix epoch (1970-01-01 00:00:00 UTC).</returns>
+    public static long ToUnix(this DateTimeOffset datetimeOffset)
+    {
+        return datetimeOffset.ToUnixTimeSeconds();
+    }
+
+    /// <summary>
+    /// Converts a Unix timestamp (number of seconds since Unix epoch) to its equivalent <see cref="DateTimeOffset"/>.
+    /// </summary>
+    /// <param name="seconds">The Unix timestamp to convert.</param>
+    /// <returns>A <see cref="DateTimeOffset"/> object representing the date and time in the utc time zone.</returns>
+    /// <remarks>
+    /// The <see cref="DateTimeOffset.Offset"/> property value of the returned <see cref="DateTimeOffset"/> instance is <see cref="TimeSpan.Zero"/>, which represents Coordinated Universal Time.
+    /// </remarks>
+    public static DateTimeOffset ToDateTimeOffset(this long seconds)
+    {
+        return DateTimeOffset.FromUnixTimeSeconds(seconds);
     }
 }
--- test/Alyio.Extensions.Tests/DateTimeExtensionsTests.cs
+++ test/Alyio.Extensions.Tests/DateTimeExtensionsTests.cs
@@ -6,56 +6,52 @@ namespace Alyio.Extensions.Tests
 {
     public class DateTimeExtensionsTests
     {
-        [Theory]
-        [InlineData(2025, 9, 15, 20250915)]
-        [InlineData(2024, 2, 29, 20240229)]
-        [InlineData(1999, 12, 31, 19991231)]
-        public void ToDateInt32_Should_Return_Correct_Integer(int year, int month, int day, int expected)
+        [Fact]
+        public void ToDateInt32_FromDateTime_ReturnsCorrectInteger()
         {
-            var dt = new DateTime(year, month, day);
+            var dt = new DateTime(2006, 1, 2);
+            var expected = 20060102;
             Assert.Equal(expected, dt.ToDateInt32());
         }
 
         [Fact]
-        public void ToyyyyMMddHHmmss_Should_Return_Correctly_Formatted_String()
+        public void ToyyyyMMddHHmmss_FromDateTime_ReturnsCorrectlyFormattedString()
         {
-            var dt = new DateTime(2025, 9, 15, 14, 30, 55);
-            var expected = "2025-09-15 14:30:55";
+            var dt = new DateTime(2006, 1, 2, 15, 4, 5);
+            var expected = "2006-01-02 15:04:05";
             Assert.Equal(expected, dt.ToyyyyMMddHHmmss());
         }
 
-        [Theory]
-        [InlineData(1497002400L, 2017, 6, 9, 10, 0, 0)] // Original test case
-        [InlineData(0L, 1970, 1, 1, 0, 0, 0)]           // Unix epoch
-        [InlineData(1663200000L, 2022, 9, 15, 0, 0, 0)]
-        public void ToUnix_And_ToDateTime_FromLong_Should_Convert_Correctly(long unixTimestamp, int year, int month, int day, int hour, int min, int sec)
+        [Fact]
+        public void ToUnix_And_ToDateTime_FromLong_RoundTripConversion_IsCorrect()
         {
+            long timestamp = 1136214245L;
+            var expectedDateTime = new DateTime(2006, 1, 2, 15, 4, 5, DateTimeKind.Utc);
+
             // Test long -> DateTime
-            var expectedDateTime = new DateTime(year, month, day, hour, min, sec, DateTimeKind.Utc);
-            var actualDateTime = unixTimestamp.ToDateTime();
+            var actualDateTime = timestamp.ToDateTime();
             Assert.NotNull(actualDateTime);
-            Assert.Equal(expectedDateTime, actualDateTime.Value.ToUniversalTime());
+            Assert.Equal(expectedDateTime, actualDateTime);
 
             // Test DateTime -> long
-            var actualUnixTimestamp = actualDateTime.Value.ToUnix();
-            Assert.Equal(unixTimestamp, actualUnixTimestamp);
+            var actualTimestamp = actualDateTime.Value.ToUnix();
+            Assert.Equal(timestamp, actualTimestamp);
         }
 
-        [Theory]
-        [InlineData(1497002400.5, 2017, 6, 9, 10, 0, 0, 500)] // With milliseconds
-        [InlineData(0.0, 1970, 1, 1, 0, 0, 0, 0)]
-        public void ToDateTime_FromDouble_Should_Convert_Correctly(double unixTimestamp, int year, int month, int day, int hour, int min, int sec, int ms)
+        [Fact]
+        public void ToDateTime_FromDouble_ReturnsCorrectDateTime()
         {
-            var expectedDateTime = new DateTime(year, month, day, hour, min, sec, ms, DateTimeKind.Utc);
-            var actualDateTime = unixTimestamp.ToDateTime();
+            double timestamp = 1136214245.999;
+            var expectedDateTime = new DateTime(2006, 1, 2, 15, 4, 5, 999, DateTimeKind.Utc);
+            var actualDateTime = timestamp.ToDateTime();
             Assert.NotNull(actualDateTime);
-            Assert.Equal(expectedDateTime, actualDateTime.Value.ToUniversalTime());
+            Assert.Equal(expectedDateTime, actualDateTime);
         }
 
         [Theory]
         [InlineData(-1L)]
         [InlineData(long.MinValue)]
-        public void ToDateTime_FromInvalidLong_Should_Return_Null(long invalidTimestamp)
+        public void ToDateTime_FromInvalidLong_ReturnsNull(long invalidTimestamp)
         {
             Assert.Null(invalidTimestamp.ToDateTime());
         }
@@ -65,9 +61,142 @@ namespace Alyio.Extensions.Tests
         [InlineData(double.NaN)]
         [InlineData(double.PositiveInfinity)]
         [InlineData(double.NegativeInfinity)]
-        public void ToDateTime_FromInvalidDouble_Should_Return_Null(double invalidTimestamp)
+        public void ToDateTime_FromInvalidDouble_ReturnsNull(double invalidTimestamp)
         {
             Assert.Null(invalidTimestamp.ToDateTime());
         }
+
+        #region DateTimeOffset Tests
+
+        [Fact]
+        public void ToDateTimeOffset_FromLocalDateTime_ReturnsCorrectDateTimeOffset()
+        {
+            var dateTime = new DateTime(2006, 1, 2, 15, 4, 5, DateTimeKind.Local);
+            var dateTimeOffset = dateTime.ToDateTimeOffset();
+
+            Assert.Equal(dateTime, dateTimeOffset.LocalDateTime);
+            Assert.Equal(TimeZoneInfo.Local.BaseUtcOffset, dateTimeOffset.Offset);
+        }
+
+        [Fact]
+        public void ToDateTimeOffset_FromUnspecifiedDateTime_ReturnsCorrectDateTimeOffset()
+        {
+            var dateTime = new DateTime(2006, 1, 2, 15, 4, 5, DateTimeKind.Unspecified);
+            var dateTimeOffset = dateTime.ToDateTimeOffset();
+
+            Assert.Equal(dateTime, dateTimeOffset.LocalDateTime);
+            Assert.Equal(TimeZoneInfo.Local.BaseUtcOffset, dateTimeOffset.Offset);
+        }
+
+        [Fact]
+        public void ToDateTimeOffset_FromUtcDateTime_HandlesCorrectly()
+        {
+            var utcDateTime = new DateTime(2006, 1, 2, 15, 4, 5, DateTimeKind.Utc);
+            var utcOffset = utcDateTime.ToDateTimeOffset();
+            Assert.Equal(utcDateTime, utcOffset.DateTime);
+            Assert.Equal(TimeSpan.Zero, utcOffset.Offset);
+        }
+
+        [Theory]
+        [InlineData(2006, 1, 2, 15, 4, 5, 0, 0)] // UTC
+        [InlineData(2006, 1, 2, 15, 4, 5, 5, 30)]  // +05:30 offset
+        [InlineData(2006, 1, 2, 15, 4, 5, -8, 0)] // -08:00 offset
+        public void ToDateTime_FromDateTimeOffset_ConvertsToUtc(int year, int month, int day, int hour, int min, int sec, int offsetHours, int offsetMinutes)
+        {
+            var offset = new TimeSpan(offsetHours, offsetMinutes, 0);
+            var dateTimeOffset = new DateTimeOffset(year, month, day, hour, min, sec, offset);
+            var utcDateTime = dateTimeOffset.ToDateTime();
+
+            Assert.Equal(dateTimeOffset.UtcDateTime, utcDateTime);
+            Assert.Equal(DateTimeKind.Utc, utcDateTime.Kind);
+        }
+
+        [Fact]
+        public void ToUnix_FromDateTimeOffset_ReturnsCorrectUnixTimestamp()
+        {
+            long expectedTimestamp = 1136214245L;
+            var dateTimeOffset = new DateTimeOffset(2006, 1, 2, 15, 4, 5, TimeSpan.Zero);
+            var actualTimestamp = dateTimeOffset.ToUnix();
+
+            Assert.Equal(expectedTimestamp, actualTimestamp);
+        }
+
+        [Fact]
+        public void ToDateTimeOffset_FromLong_ReturnsCorrectDateTimeOffset()
+        {
+            long timestamp = 1136214245L;
+            var dateTimeOffset = timestamp.ToDateTimeOffset();
+
+            Assert.Equal(2006, dateTimeOffset.Year);
+            Assert.Equal(1, dateTimeOffset.Month);
+            Assert.Equal(2, dateTimeOffset.Day);
+            Assert.Equal(15, dateTimeOffset.Hour);
+            Assert.Equal(4, dateTimeOffset.Minute);
+            Assert.Equal(5, dateTimeOffset.Second);
+            Assert.Equal(TimeSpan.Zero, dateTimeOffset.Offset);
+        }
+
+        [Fact]
+        public void ToDateTimeOffset_FromLong_WithEdgeCases_HandlesCorrectly()
+        {
+            // Test with zero timestamp (Unix epoch)
+            var epochOffset = 0L.ToDateTimeOffset();
+            Assert.Equal(1970, epochOffset.Year);
+            Assert.Equal(1, epochOffset.Month);
+            Assert.Equal(1, epochOffset.Day);
+            Assert.Equal(0, epochOffset.Hour);
+            Assert.Equal(0, epochOffset.Minute);
+            Assert.Equal(0, epochOffset.Second);
+            Assert.Equal(TimeSpan.Zero, epochOffset.Offset);
+
+            // Test with large timestamp
+            var largeTimestamp = 1136214245L; // 2006-01-02 15:04:05 UTC
+            var largeOffset = largeTimestamp.ToDateTimeOffset();
+            Assert.Equal(2006, largeOffset.Year);
+            Assert.Equal(1, largeOffset.Month);
+            Assert.Equal(2, largeOffset.Day);
+            Assert.Equal(TimeSpan.Zero, largeOffset.Offset);
+        }
+
+        [Fact]
+        public void DateTimeOffset_RoundTripConversion_WorksCorrectly()
+        {
+            // Test DateTime -> DateTimeOffset -> DateTime
+            var originalDateTime = new DateTime(2006, 1, 2, 15, 4, 5, DateTimeKind.Utc);
+            var dateTimeOffset = originalDateTime.ToDateTimeOffset();
+            var convertedDateTime = dateTimeOffset.ToDateTime();
+
+            Assert.Equal(originalDateTime.ToUniversalTime(), convertedDateTime.ToUniversalTime());
+
+            // Test long -> DateTimeOffset -> long
+            var originalUnix = 1136214245L;
+            var dateTimeOffsetFromUnix = originalUnix.ToDateTimeOffset();
+            var convertedUnix = dateTimeOffsetFromUnix.ToUnix();
+
+            Assert.Equal(originalUnix, convertedUnix);
+        }
+
+        [Fact]
+        public void ToDateTime_FromDateTimeOffset_WithDifferentOffsets_ConvertsToSameUtc()
+        {
+            var baseDateTime = new DateTime(2006, 1, 2, 15, 4, 5, DateTimeKind.Unspecified);
+            var baseUtcDateTime = DateTime.SpecifyKind(baseDateTime, DateTimeKind.Utc);
+
+            // Create DateTimeOffset instances with different offsets but same UTC time
+            var utcOffset = new DateTimeOffset(baseDateTime, TimeSpan.Zero);
+            var plus5Offset = new DateTimeOffset(baseDateTime.AddHours(-5), TimeSpan.FromHours(-5));
+            var minus8Offset = new DateTimeOffset(baseDateTime.AddHours(8), TimeSpan.FromHours(8));
+
+            // All should convert to the same UTC DateTime
+            var utcDateTime1 = utcOffset.ToDateTime();
+            var utcDateTime2 = plus5Offset.ToDateTime();
+            var utcDateTime3 = minus8Offset.ToDateTime();
+
+            Assert.Equal(baseUtcDateTime, utcDateTime1);
+            Assert.Equal(baseUtcDateTime, utcDateTime2);
+            Assert.Equal(baseUtcDateTime, utcDateTime3);
+        }
+
+        #endregion
     }
 }
